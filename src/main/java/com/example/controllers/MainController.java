95bb231e381f4fb5319253d9bcc7c9b501ab7aad
llruiz <llanosrzm@gmail.com>
2023-03-22 11:59:48 +0100

    método actualizar estudiante

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -100,6 +101,14 @@ public class MainController {
 
     }
     
+/**
+ * Muestra el formulario para Actualizar un Estudiante
+ */
 
+ @GetMapping("/frmActualizar/{id}")
+ public String frmActualizarEstudiante(@PathVariable(name= "id") int idEstudiante){
+
+    return "redirect:/listar";
+ }
     }
 
