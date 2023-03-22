ba5fe41a6b6bc9c0d44a45103541aa2ae1b15ff9
llruiz <llanosrzm@gmail.com>
2023-03-22 10:56:12 +0100

    Insertar telefonos

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,29 +1,40 @@
 package com.example.controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.entities.Estudiante;
 import com.example.entities.Facultad;
+import com.example.entities.Telefono;
 import com.example.services.EstudianteService;
 import com.example.services.FacultadService;
+import com.example.services.TelefonoService;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    private static final Logger LOG = Logger.getLogger("MainController");
 
     @Autowired
     private EstudianteService estudianteService;
 
     @Autowired
     private FacultadService facultadService;
+
+    @Autowired
+    private TelefonoService telefonoService;
 
     /**
      * El metodo siguiente devuelve un listado de estudiantes
@@ -57,7 +68,38 @@ public class MainController {
      * Metodo que recibe los datos procedente de los controles del formulario
      */
     @PostMapping("/altaEstudiante")
-    public String altaEstudiante() {
-        return "views/altaEstudianteSuccessed";
+    public String altaEstudiante(@ModelAttribute Estudiante estudiante,
+                    @RequestParam(name = "numerosTelefonos") String telefonosRecibidos) {
+            
+            LOG.info("telefonos recibidos" + telefonosRecibidos);
+
+            List<String> listadoNumerosTelefonos = null;
+
+            if(telefonosRecibidos != null){
+                String[] arrayTelefonos = telefonosRecibidos.split(";");
+
+                listadoNumerosTelefonos = Arrays.asList(arrayTelefonos);
+
+            }
+
+            estudianteService.save(estudiante);
+
+            if(listadoNumerosTelefonos != null){
+                listadoNumerosTelefonos.stream().forEach(n -> {
+                    Telefono telefonoObject = Telefono
+                    .builder()
+                    .numero(n)
+                    .estudiante(estudiante) //el estudiante que se ha guardado en estudianteService
+                    .build();
+
+                    telefonoService.save(telefonoObject);
+                });               
+            }
+
+            return "redirect:/listar";
+
     }
-}
+    
+
+    }
+
